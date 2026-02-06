8dd37499236ec9addbb44f436ef6c9ecac634d7f
seryl <seryllounis@outlook.fr>
2026-02-06 18:10:25 +0100

    feat: auto-precompile engine stylesheet with Sprockets
    Register data_porter/application.css in the asset precompile list via
    an engine initializer. Add stylesheet_link_tag to show template.

--- lib/data_porter/engine.rb
+++ lib/data_porter/engine.rb
@@ -4,6 +4,10 @@ module DataPorter
   class Engine < ::Rails::Engine
     isolate_namespace DataPorter
 
+    initializer "data_porter.assets.precompile" do |app|
+      app.config.assets.precompile += %w[data_porter/application.css] if app.config.respond_to?(:assets)
+    end
+
     config.to_prepare do
       Dir[Rails.root.join("app/importers/*_target.rb")].each { |f| require f }
     end
