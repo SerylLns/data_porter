83ed77c1ff69ce7e58ae6ad9db46a6a17b9f5db5
seryl <seryllounis@outlook.fr>
2026-02-06 13:29:57 +0100

    chore: add SQLite for in-memory test database

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,32 @@
 # frozen_string_literal: true
 
 require "rails"
+require "active_record"
 require "data_porter"
+
+$LOAD_PATH.unshift File.expand_path("../app/models", __dir__)
+require "data_porter/data_import"
+
+ActiveRecord::Base.establish_connection(
+  adapter: "sqlite3",
+  database: ":memory:"
+)
+
+ActiveRecord::Schema.define do
+  create_table :data_porter_imports, force: true do |t|
+    t.string  :target_key,  null: false, default: ""
+    t.string  :source_type, null: false, default: "csv"
+    t.integer :status,      null: false, default: 0
+    t.text    :records
+    t.text    :report
+    t.text    :config
+
+    t.string  :user_type
+    t.integer :user_id
+
+    t.timestamps
+  end
+end
 
 RSpec.configure do |config|
   config.example_status_persistence_file_path = ".rspec_status"
